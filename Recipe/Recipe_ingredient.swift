69ba6c3479983a95d9aa3246e6af4129f3de7970
Raven <1772133019@qq.com>
2022-09-04 22:11:20 +0800

    2211

--- Recipe/Recipe_ingredient.swift
+++ Recipe/Recipe_ingredient.swift
@@ -7,11 +7,40 @@
 
 import SwiftUI
 
-class ingredient
+// 继承的这两个特性可以使 name, checked 等变量保存下来
+class ingredient: NSObject, NSCoding
 {
+    func encode(with coder: NSCoder) {
+        // encode 要保存的变量，
+        coder.encode(self.name, forKey: "name")
+        coder.encode(self.checked, forKey: "checked")
+    }
+    
+    required init?(coder: NSCoder) {
+        self.name    = coder.decodeObject(forKey: "name") as? String ?? ""
+        self.checked = coder.decodeBool(forKey: "checked")
+    }
+    
     var name:String   = ""
-    var 
+    var checked:Bool  = false
+    var i:Int         = 0
+    
+    // 应该添加图标，可以用 Emoji
+//    var icon:IconOnlyLabelStyle = 1
+    
+    // 如果要添加日期的话，这里可以写
+//    var dueDate:Date  = Date()
+
+    
+    init(name:String)
+    {
+        self.name = name
+    }
 }
+
+
+
+
 
 
 struct Recipe_ingredient: View {
