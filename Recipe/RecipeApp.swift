5b3649c27ffdd680729d2e8540168a6169e96970
Raven <1772133019@qq.com>
2022-09-05 00:25:36 +0800

    0025

--- Recipe/RecipeApp.swift
+++ Recipe/RecipeApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 @main
+
 struct RecipeApp: App {
     var body: some Scene {
         WindowGroup {
--- Recipe/SceneDelegate.swift
+++ Recipe/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: Home(main: Main()))
+            // window.rootViewController = UIHostingController(rootView: home(main: main()))
             self.window = window
             window.makeKeyAndVisible()
         }
