c8dfc14e9b54eb7e3e100d08f443d759e489e121
Raven <1772133019@qq.com>
2022-09-04 21:17:28 +0800

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Ravin on 2022/9/4.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
